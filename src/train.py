b20124ab6b1c80e446638bc28f2f33fe26a912b2
yaredcomp <yaredderejeamu@gmail.com>
2022-11-28 19:49:49 +0300

    final commit

--- src/train.py
+++ src/train.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 from skimage.io import imread_collection
 from skimage.transform import resize
-from sklearn.linear_model import SGDClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 
 def load_images(data_frame, column_name):
@@ -32,11 +32,11 @@ def load_data(data_path):
     return data, labels
 
 
-def main(repo_path):
+def main(path_to_repo):
     train_csv_path = repo_path / "data/prepared/train.csv"
     train_data, labels = load_data(train_csv_path)
-    sgd = SGDClassifier(max_iter=10)
-    trained_model = sgd.fit(train_data, labels)
+    rf = RandomForestClassifier()
+    trained_model = rf.fit(train_data, labels)
     dump(trained_model, repo_path / "model/model.joblib")
 
 
